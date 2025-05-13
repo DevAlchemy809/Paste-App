dacee5fbfd35001b76cb280064e43cbaa68bd5e1
webdev123135 <krishna111100@gmail.com>
2025-05-13 16:10:30 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './Paste-App/store.js';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
       <Toaster />
   </Provider>
 )
