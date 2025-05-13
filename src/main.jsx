8b84615bf7ffab71924029457f435da5b83dc447
webdev123135 <krishna111100@gmail.com>
2025-05-13 13:08:29 +0530

    Fix Vite base path for Vercel deployment (blank page issue)

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import App from './App.jsx'
 import { store } from './Paste-App/store.js';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
       <Toaster />
   </Provider>
 )
